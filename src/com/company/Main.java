8d7b0c274f0974f60aab9d94a99afcc6f4cffed0
KelvemC <kelvemcif@gmail.com>
2022-01-28 00:11:33 -0300

    Erros Concertados.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Cliente venilton = new Cliente();
-        venilton.setNome("Venilton");
+        Cliente Kelvem = new Cliente();
+        Kelvem.setNome("Kelvem");
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        Conta cc = new ContaCorrente(Kelvem);
+        Conta poupanca = new ContaPoupanca(Kelvem);
 
         cc.depositar(500);
         cc.transferir(500, poupanca);
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
         //Compra 1
-        /*System.out.println("Fazendo compra de um livro: ");
-        poupanca.fazerCompraDebito("Livro: Use a cabeça!: Java", 135.7);*/
+        System.out.println("Fazendo compra de um livro: ");
+        poupanca.fazerCompraDebito("Livro: Use a cabeça!: Java", 135.7);
         System.out.println("\n");
         System.out.println("Fazendo compra de uma cama por crédito!");
         poupanca.fazerCompraCredito("Cama Box Casal (Box + Colchão) Plumatex Mola", 899.5);
